bdf67482a7d78c4bf72a9fa0692e7f80a8bb129b
subhamManU <subhamsaha90@gmail.com>
2020-08-22 15:39:20 +0530

    First Commit

---
+++ src/main/java/com/self/learning/springBootAppWithTDD/SpringBootAppWithTddApplication.java
@@ -0,0 +1,13 @@
+package com.self.learning.springBootAppWithTDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppWithTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppWithTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/self/learning/springBootAppWithTDD/SpringBootAppWithTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.learning.springBootAppWithTDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppWithTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
